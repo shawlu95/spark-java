9613e08f011690e955123a3845c2d28f0df26508
Shaw Lu <shawlu95@gmail.com>
2022-11-07 22:29:27 -0800

    spark sql from csv data

--- src/main/java/com/sparksql/Main.java
+++ src/main/java/com/sparksql/Main.java
@@ -2,55 +2,69 @@ package com.sparksql;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.col;
 
 public class Main {
 
 	@SuppressWarnings("resource")
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.setProperty("hadoop.home.dir", "c:/hadoop");
 		Logger.getLogger("org.apache").setLevel(Level.WARN);
-		
-		SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
-		JavaSparkContext sc = new JavaSparkContext(conf);
-		
-		JavaRDD<String> initialRdd = sc.textFile("src/main/resources/subtitles/input.txt");
-		
-		JavaRDD<String> lettersOnlyRdd = initialRdd.map( sentence -> sentence.replaceAll("[^a-zA-Z\\s]", "").toLowerCase() );
-		
-		JavaRDD<String> removedBlankLines = lettersOnlyRdd.filter( sentence -> sentence.trim().length() > 0 );
-		
-		JavaRDD<String> justWords = removedBlankLines.flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator());
-		
-		JavaRDD<String> blankWordsRemoved = justWords.filter(word -> word.trim().length() > 0);
-		
-		JavaRDD<String> justInterestingWords = blankWordsRemoved.filter(word -> Util.isNotBoring(word));
-		
-		JavaPairRDD<String, Long> pairRdd = justInterestingWords.mapToPair(word -> new Tuple2<String, Long>(word, 1L));
-		
-		JavaPairRDD<String, Long> totals = pairRdd.reduceByKey((value1, value2) -> value1 + value2);
-		
-		JavaPairRDD<Long, String> switched = totals.mapToPair(tuple -> new Tuple2<Long, String> (tuple._2, tuple._1 ));
-		
-		JavaPairRDD<Long, String> sorted = switched.sortByKey(false);
-		
-		List<Tuple2<Long,String>> results = sorted.take(10);
-		
-		results.forEach(System.out::println);		
-		
-		Scanner scanner = new Scanner(System.in);
-		scanner.nextLine();
+		Logger logger = Logger.getLogger(Main.class);
 
-		sc.close();
+		SparkSession sc = SparkSession.builder()
+				.appName("sparksql practice")
+				.master("local[*]")
+				.getOrCreate();
+
+		Dataset<Row> dataset = sc.read()
+				.option("header", true)
+				.csv("src/main/resources/exams/students.csv");
+
+		dataset.show();
+		logger.info("Number of rows: " + dataset.count());
+
+		Row first = dataset.first();
+		logger.info(first.get(0));
+		logger.info(first.get(1));
+		logger.info(first.get(first.fieldIndex("subject")).toString());
+		logger.info(first.get(first.fieldIndex("year")));
+		logger.info(first.getAs("grade").toString());
+
+		// all internal types of csv are string, auto conversion doesn't work
+		// int year = first.getAs("year");
+		int year_ = Integer.parseInt(first.getAs("year"));
+
+		// -----------------------------------------------------------------
+		// HOW TO FILTER
+		Dataset<Row> math = dataset.filter("subject = 'Math' and score > 70");
+		math.show(10);
+
+		// use function, may be clumsy
+		// Dataset<Row> top = dataset.filter((Function1<Row, Object>) x -> x.getAs("grade").equals("grade"));
+		// top.show(10);
+
+		// spark sql idiom
+		Column subject = dataset.col("subject");
+		Column year = dataset.col("year");
+		Dataset<Row> modernArt = dataset.filter(subject.equalTo("Modern Art").and(year.equalTo(2007)));
+		modernArt.show(10);
+
+		// or import a static method
+		Column subjectCol = col("subject");
+		Column yearCol = col("year");
+		dataset.filter(col("subject").equalTo("Math").and(col("year").gt(2010))).show(10);
+
+		// -----------------------------------------------------------------
+		// FULL SQL SYNTAX
+		dataset.createOrReplaceTempView("students");
+		Dataset<Row> agg = sc.sql("select subject, count(*) from students where grade = 'A+' group by subject");
+		agg.show(100);
+
+		sc.sql("select year, count(distinct student_id) as student_count from students group by year order by year").show(100);
 	}
-
 }
