a4e5cccbcde1951bec7df5fc4da596cc5b93df6c
Shaw Lu <shawlu95@gmail.com>
2022-11-29 23:39:58 -0800

    Create LoggingServer.java

---
+++ src/main/java/com/stream/LoggingServer.java
@@ -0,0 +1,34 @@
+package com.stream;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Simulate a streaming source from a web server
+ * A basic example. Nowhere near production standard.
+ * Run as a regular Java application. Expect nothing in console log. */
+public class LoggingServer {
+	public static void main(String[] args) throws IOException, InterruptedException {
+	    ServerSocket echoSocket = new ServerSocket(8989);
+	    Socket socket = echoSocket.accept();
+
+	    while (true) {
+	    	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+	    	double sample = Math.random();
+	    	String level = "DEBUG";
+	    	if (sample < 0.0001) {
+	    		level ="FATAL";
+	    	} else if (sample < 0.01) {
+	    		level = "ERROR";
+	    	} else if (sample < 0.1) {
+	    		level = "WARN";
+	    	} else if (sample < 0.5) {
+	    		level = "INFO";
+	    	}
+	    	out.println(level + "," + new java.util.Date());
+	    	Thread.sleep(1);
+	    }
+	}
+}
