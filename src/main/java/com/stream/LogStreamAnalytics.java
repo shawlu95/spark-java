84ec28d47592456351dc01c735a54b9bdad2e712
Shaw Lu <shawlu95@gmail.com>
2022-11-30 00:35:26 -0800

    Count log by mini-batch

--- src/main/java/com/stream/LogStreamAnalytics.java
+++ src/main/java/com/stream/LogStreamAnalytics.java
@@ -5,8 +5,10 @@ import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
 
 public class LogStreamAnalytics {
     public static void main(String[] args) throws InterruptedException {
@@ -22,7 +24,11 @@ public class LogStreamAnalytics {
 
         // a discretized, long series of RDD, but works just like a regular RDD
         JavaDStream<String> results = inputData.map(item -> item);
-        results.print();
+        JavaPairDStream<String, Long> pairDStream = results
+                .map(msg -> msg.split(",")[0])
+                .mapToPair(msg -> new Tuple2<String, Long>(msg, 1L))
+                .reduceByKey(Long::sum);
+        pairDStream.print();
 
         jsc.start();
         jsc.awaitTermination(); // manual termination
