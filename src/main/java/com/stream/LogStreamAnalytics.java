84b473ecbebd5c3b8c4b6fc5d5b86abd58e47221
Shaw Lu <shawlu95@gmail.com>
2022-12-01 00:16:07 -0800

    aggregate by window longer than batches

--- src/main/java/com/stream/LogStreamAnalytics.java
+++ src/main/java/com/stream/LogStreamAnalytics.java
@@ -18,16 +18,20 @@ public class LogStreamAnalytics {
         SparkConf conf = new SparkConf()
                 .setMaster("local[*]")
                 .setAppName("LogStreamAnalytics");
+
+        // define batch size (duration) at context
         JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(5));
         JavaReceiverInputDStream<String> inputData =
                 jsc.socketTextStream("localhost", 8989);
 
         // a discretized, long series of RDD, but works just like a regular RDD
+        // every 5 seconds, a batch is processed, and the stream is aggregated over last 24 batches
+        // i.e. 24 * 5s-batches in a two-minute window
         JavaDStream<String> results = inputData.map(item -> item);
         JavaPairDStream<String, Long> pairDStream = results
                 .map(msg -> msg.split(",")[0])
                 .mapToPair(msg -> new Tuple2<String, Long>(msg, 1L))
-                .reduceByKey(Long::sum);
+                .reduceByKeyAndWindow(Long::sum, Durations.minutes(2)); // define window at aggregation
         pairDStream.print();
 
         jsc.start();
