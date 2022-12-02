e3f3639f3eb351af0826b730adbd49103f83ab2e
Shaw Lu <shawlu95@gmail.com>
2022-12-01 17:12:43 -0800

    create kafka consumer ViewingFiguresDStream

---
+++ src/main/java/com/stream/kafka/ViewingFiguresDStream.java
@@ -0,0 +1,51 @@
+package com.stream.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ViewingFiguresDStream {
+    public static void main(String[] args) throws InterruptedException {
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+        Logger.getLogger("org.apache.spark.storage").setLevel(Level.ERROR);
+
+        SparkConf conf = new SparkConf()
+                .setMaster("local[*]")
+                .setAppName("ViewingFiguresDStream");
+
+        // define batch size (duration) at context
+        JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(1));
+
+        Collection<String> topics = Arrays.asList("viewrecords");
+        Map<String, Object> params = new HashMap<>();
+        params.put("bootstrap.servers", "localhost:9092");
+        params.put("key.deserializer", StringDeserializer.class);
+        params.put("value.deserializer", StringDeserializer.class);
+        params.put("group.id", "group_id");
+        params.put("auto.offset.reset", "latest");
+        params.put("enable.auto.commit", true);
+
+        JavaInputDStream<ConsumerRecord<String, String>> stream =
+                KafkaUtils.createDirectStream(jsc,
+                    LocationStrategies.PreferConsistent(),
+                    ConsumerStrategies.Subscribe(topics, params));
+
+        stream.map(item -> item.value()).print();
+
+        jsc.start();
+        jsc.awaitTermination();
+    }
+}
