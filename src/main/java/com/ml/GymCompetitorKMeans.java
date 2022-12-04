52817f77a2f10b940994af157ec16e897ac74779
Shaw Lu <shawlu95@gmail.com>
2022-12-04 11:04:01 -0800

    train K-means

--- src/main/java/com/ml/GymCompetitorKMeans.java
+++ src/main/java/com/ml/GymCompetitorKMeans.java
@@ -2,8 +2,12 @@ package com.ml;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.ml.clustering.KMeans;
+import org.apache.spark.ml.clustering.KMeansModel;
 import org.apache.spark.ml.feature.OneHotEncoderEstimator;
 import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.linalg.Vector;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -31,5 +35,22 @@ public class GymCompetitorKMeans {
                 .setInputCols(new String[] { "GenderIndex" })
                 .setOutputCols(new String[] { "GenderVector" });
         csv = genderEncoder.fit(csv).transform(csv);
+
+        // all inputs to VectorAssembler must be numeric, not string
+        VectorAssembler assembler = new VectorAssembler()
+                .setInputCols(new String[]{ "GenderVector", "Age", "Height", "Weight", "NoOfReps" })
+                .setOutputCol("features");
+        Dataset<Row> dataset = assembler
+                .transform(csv)
+                .select("features");
+
+        // we don't separate train & test set with unsupervised learning
+        KMeans kMeans = new KMeans().setK(3);
+        KMeansModel model = kMeans.fit(dataset);
+        Dataset<Row> pred = model.transform(dataset);
+        pred.show(10);
+
+        Vector[] clusterCenters = model.clusterCenters();
+        for (Vector v : clusterCenters) { System.out.println(v); };
     }
 }
