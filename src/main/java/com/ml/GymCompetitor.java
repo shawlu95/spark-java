09f60033dfb1210ba8d5ae9dd33171e18fb3267b
Shaw Lu <shawlu95@gmail.com>
2022-12-03 17:22:04 -0800

    one-hot encode gender features (indexing and encoding)

--- src/main/java/com/ml/GymCompetitor.java
+++ src/main/java/com/ml/GymCompetitor.java
@@ -2,6 +2,8 @@ package com.ml;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.ml.feature.OneHotEncoderEstimator;
+import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.linalg.Vector;
 import org.apache.spark.ml.regression.LinearRegression;
@@ -23,6 +25,33 @@ public class GymCompetitor {
                 .option("header", true)
                 .option("inferSchema", true)
                 .csv("src/main/resources/ml/GymCompetition.csv");
+
+        /* apply indexing to categorical feature
+        +------------+------+---+------+------+--------+-----------+
+        |CompetitorID|Gender|Age|Height|Weight|NoOfReps|GenderIndex|
+        +------------+------+---+------+------+--------+-----------+
+        |           1|     M| 23|   180|    88|      55|        0.0|
+        |           5|     F| 24|   180|    78|      52|        1.0|
+        |           6|     M| 25|   171|    89|      60|        0.0|
+        |           7|     F| 18|   179|    80|      54|        1.0|
+        +------------+------+---+------+------+--------+-----------+*/
+        StringIndexer genderIndexer = new StringIndexer()
+                .setInputCol("Gender")
+                .setOutputCol("GenderIndex");
+        csv = genderIndexer.fit(csv).transform(csv);
+
+        /* transfer indexed feature to one-hot vector
+        |CompetitorID|Gender|Age|Height|Weight|NoOfReps|GenderIndex| GenderVector|
+        +------------+------+---+------+------+--------+-----------+-------------+
+        |           1|     M| 23|   180|    88|      55|        0.0|(1,[0],[1.0])|
+        |           5|     F| 24|   180|    78|      52|        1.0|    (1,[],[])|
+        |           6|     M| 25|   171|    89|      60|        0.0|(1,[0],[1.0])|
+        |           7|     F| 18|   179|    80|      54|        1.0|    (1,[],[])|
+        +------------+------+---+------+------+--------+-----------+-------------+*/
+        OneHotEncoderEstimator genderEncoder = new OneHotEncoderEstimator()
+                .setInputCols(new String[] { "GenderIndex" })
+                .setOutputCols(new String[] { "GenderVector" });
+        csv = genderEncoder.fit(csv).transform(csv);
 
         // all inputs to VectorAssembler must be numeric, not string
         VectorAssembler assembler = new VectorAssembler();
